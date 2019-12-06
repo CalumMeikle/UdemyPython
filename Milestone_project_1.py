77bca905f94c471e9e8c02828c9ba31b143e0eb2
CalumMeikle <calum.meikle@k-int.co.uk>
2019-12-06 16:55:35 +0000

    project1

---
+++ Milestone_project_1.py
@@ -0,0 +1,10 @@
+'''
+A milestone project for the Udemy course to make a tic tac toe game. Up tho this point the content covered is statements and functions.
+'''
+
+def the_board(first=" ", second=" ", third=" ", fourth=" ", fifth=" ", sixth=" ", seventh=" ", eighth=" ", ninth=" "):
+    return f"{first}|{second}|{third}\n_|_|_\n{fourth}|{fifth}|{sixth}\n_|_|_\n{seventh}|{eighth}|{ninth}\n | | "
+
+print(the_board())
+
+
